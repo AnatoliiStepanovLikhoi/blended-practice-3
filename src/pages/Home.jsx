b81a4bc9a348b02abd1d07b5c848a647955204c6
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-01-28 19:11:41 +0200

    add task 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useState, useEffect } from 'react';
+import { getCountries } from '../service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const fetchCountries = async () => {
+      try {
+        const countryList = await getCountries();
+
+        setCountries(countryList);
+
+        // console.log(countryList);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        {loading && <Loader />}
+        {countries && <CountryList countries={countries} />}
+        {error && <Heading>Ops, something went wrong!</Heading>}
       </Container>
     </Section>
   );
